2a31e9e31b87d006b2a080843e4e9f24fc6a5005
hemantafpv <87780753+hemantafpv@users.noreply.github.com>
2021-09-26 22:09:20 -0400

    Create interface.java

---
+++ interface.java
@@ -0,0 +1,24 @@
+package chapter3;
+
+public interface PileInterface<T> {
+	/** Adds a new book to the top of this pile.
+	 @param newBook  An object to be added to the pile. */
+public void add(T newBook);
+
+/** Removes and returns this pile's top book.
+	 @return  Either the book at the top of the pile or, if the
+			    pile is empty before the operation, null. */
+public T remove();
+
+/** Retrieves this pile's top book.
+	 @return  Either the book at the top of the pile or null if
+			    the pile is empty. */
+public T getTopBook();
+
+/** Detects whether this pile is empty.
+	 @return  True if the pile is empty. */
+public boolean isEmpty();
+
+/** Removes all books from this pile. */
+public void clear();
+}
